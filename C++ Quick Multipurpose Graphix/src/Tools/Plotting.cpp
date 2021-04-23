bfd096dda7ed89db7d26586773509272c85c2c6b
Aathreya Kadambi <42982913+AathreyaK@users.noreply.github.com>
2021-04-23 10:45:12 -0400

    Removed 2D point plotting functions

--- C++ Quick Multipurpose Graphix/src/Tools/Plotting.cpp
+++ C++ Quick Multipurpose Graphix/src/Tools/Plotting.cpp
@@ -1,43 +1,6 @@
 #include "Plotting.h"
 
-Mesh Plotting::plot2DPoints(std::queue<float> pos, std::function<glm::vec4(glm::vec2)> colF) // I'm p sure this is deffective, the pos thing should be stored in triples
-{
-	std::queue<float> cols;
-	std::queue<float> temp = pos;
-	while (!temp.empty())
-	{
-		float a, b;
-		a = temp.front(); temp.pop();
-		b = temp.front(); temp.pop();
-		glm::vec4 col = colF(glm::vec2(a, b));
-		for (int i = 0; i < 4; i++)
-			cols.push(col[i]);
-	}
-	return Mesh(pos, cols, std::vector<int>(), CQUMUG_MESH_3D, 0, 1);
-}
-
-Mesh Plotting::plot2DPoints(std::queue<glm::vec2> pos, std::function<glm::vec4(glm::vec2)> colF)
-{
-	std::queue<float> poss;
-	std::queue<float> cols;
-	while (!pos.empty())
-	{
-		poss.push(pos.front().x);
-		poss.push(pos.front().y);
-		poss.push(0.0f); // ? Maybe ?
-
-		glm::vec4 col = colF(pos.front());
-		cols.push(col.x);
-		cols.push(col.y);
-		cols.push(col.z);
-		cols.push(col.w);
-
-		pos.pop();
-	}
-	return Mesh(poss, cols, std::vector<int>(), CQUMUG_MESH_3D, 0, 1);
-}
-
-Mesh Plotting::plot3DPoints(std::queue<float> pos, std::function<glm::vec4(glm::vec3)> colF)
+Mesh Plotting::plotPoints(std::queue<float> pos, std::function<glm::vec4(glm::vec3)> colF)
 {
 	std::queue<float> cols;
 	std::queue<float> temp = pos;
@@ -54,7 +17,7 @@ Mesh Plotting::plot3DPoints(std::queue<float> pos, std::function<glm::vec4(glm::
 	return Mesh(pos, cols, std::vector<int>(), CQUMUG_MESH_3D, 0, 1);
 }
 
-Mesh Plotting::plot3DPoints(std::queue<glm::vec3> pos, std::function<glm::vec4(glm::vec3)> colF)
+Mesh Plotting::plotPoints(std::queue<glm::vec3> pos, std::function<glm::vec4(glm::vec3)> colF)
 {
 	std::queue<float> poss;
 	std::queue<float> cols;
@@ -97,7 +60,7 @@ Mesh Plotting::plotRecurrence(glm::vec3 start, std::function<glm::vec3(glm::vec3
 	return Mesh(p, c, std::vector<int>(), CQUMUG_MESH_3D, 0, 1);
 }
 
-Mesh Plotting::plotFunction(bool x, bool y, bool z, float xl, float xr, float yl, float yr, float zl, float zr, float xs, float ys, float zs, std::function<float(glm::vec2)> f) // mark the function of the other with true (x(y, z)) would be (true, false, false)
+Mesh Plotting::plotFunction(bool x, bool y, bool z, float xl, float xr, float yl, float yr, float zl, float zr, float xs, float ys, float zs, std::function<float(glm::vec2)> f, std::function<glm::vec4(glm::vec3)> colF) // mark the function of the other with true (x(y, z)) would be (true, false, false)
 {
 	std::queue<float> p;
 	std::queue<float> c;
@@ -107,9 +70,16 @@ Mesh Plotting::plotFunction(bool x, bool y, bool z, float xl, float xr, float yl
 		{
 			for (float j = zl; j < zr; j += zs)
 			{
-				p.push(f(glm::vec2(i, j)));
+				float v = f(glm::vec2(i, j));
+				p.push(v);
 				p.push(i);
 				p.push(j);
+
+				glm::vec4 col = colF(glm::vec3(v, i, j));
+				c.push(col.x);
+				c.push(col.y);
+				c.push(col.z);
+				c.push(col.w);
 			}
 		}
 	}
@@ -119,9 +89,16 @@ Mesh Plotting::plotFunction(bool x, bool y, bool z, float xl, float xr, float yl
 		{
 			for (float j = zl; j < zr; j += zs)
 			{
+				float v = f(glm::vec2(i, j));
 				p.push(i);
-				p.push(f(glm::vec2(i, j)));
+				p.push(v);
 				p.push(j);
+
+				glm::vec4 col = colF(glm::vec3(i, v, j));
+				c.push(col.x);
+				c.push(col.y);
+				c.push(col.z);
+				c.push(col.w);
 			}
 		}
 	}
@@ -131,9 +108,16 @@ Mesh Plotting::plotFunction(bool x, bool y, bool z, float xl, float xr, float yl
 		{
 			for (float j = yl; j < yr; j += ys)
 			{
+				float v = f(glm::vec2(i, j));
 				p.push(i);
 				p.push(j);
-				p.push(f(glm::vec2(i, j)));
+				p.push(v);
+
+				glm::vec4 col = colF(glm::vec3(i, j, v));
+				c.push(col.x);
+				c.push(col.y);
+				c.push(col.z);
+				c.push(col.w);
 			}
 		}
 	}
--- C++ Quick Multipurpose Graphix/src/Tools/Plotting.h
+++ C++ Quick Multipurpose Graphix/src/Tools/Plotting.h
@@ -6,12 +6,10 @@
 
 namespace Plotting
 {
-	Mesh plot2DPoints(std::queue<float> pos, std::function<glm::vec4(glm::vec2)> colF);
-	Mesh plot2DPoints(std::queue<glm::vec2> pos, std::function<glm::vec4(glm::vec2)> colF);
-	Mesh plot3DPoints(std::queue<float> pos, std::function<glm::vec4(glm::vec3)> colF);
-	Mesh plot3DPoints(std::queue<glm::vec3> pos, std::function<glm::vec4(glm::vec3)> colF);
+	Mesh plotPoints(std::queue<float> pos, std::function<glm::vec4(glm::vec3)> colF);
+	Mesh plotPoints(std::queue<glm::vec3> pos, std::function<glm::vec4(glm::vec3)> colF);
 
 	Mesh plotRecurrence(glm::vec3 start, std::function<glm::vec3(glm::vec3)> f, uint64_t n, std::function<glm::vec4(glm::vec3)> colF);
 
-	Mesh plotFunction(bool x, bool y, bool z, float xl, float xr, float yl, float yr, float zl, float zr, float xs, float ys, float zs, std::function<float(glm::vec2)> f);
+	Mesh plotFunction(bool x, bool y, bool z, float xl, float xr, float yl, float yr, float zl, float zr, float xs, float ys, float zs, std::function<float(glm::vec2)> f, std::function<glm::vec4(glm::vec3)> colF);
 }
